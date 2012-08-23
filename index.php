d317e070d94cc664cd8a8c37e6aeec9d7956f3c6
fishinbox <397273499@qq.com>
2012-08-23 22:25:16 +0800

    add php session entry point
    Signed-off-by: fishinbox <397273499@qq.com>

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+	if(!isset($_SESSTION['sessioned']))
+	{
+		session_start();
+		$_SESSION['sessioned']=true;
+	}
+?>
 <button onclick="location='/99sushe.php'">我想通过99宿舍查询</button>
 <br />
 <button onclick="location='/chsi.php'">我想通过学信网查询</button>
+<p>欲求源代码，<a href='https://github.com/fishinbox/CET-Result-Checkout'>请见【本项目GitHub版本链接】</a></p>
